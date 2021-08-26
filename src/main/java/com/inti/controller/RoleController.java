ce7722261b054df15c3c59bda6b7509c17127aa9
RomualdLeb <lebon.romuald@hotmail.fr>
2021-08-26 14:31:30 +0200

    annotation crossOrigin

--- src/main/java/com/inti/controller/RoleController.java
+++ src/main/java/com/inti/controller/RoleController.java
@@ -3,6 +3,7 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.inti.entity.Role;
 import com.inti.service.interfaces.IRoleService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/gestionRole")
 public class RoleController {
--- src/main/java/com/inti/controller/UtilisateurController.java
+++ src/main/java/com/inti/controller/UtilisateurController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.inti.entity.Utilisateur;
 import com.inti.service.interfaces.IUtilisateurService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/gestionUtilisateur")
 public class UtilisateurController {
