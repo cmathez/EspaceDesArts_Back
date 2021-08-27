7f6e3cfd0c7ee4a869114fc9a11e6c509055cb0a
cmathez <celinemathez@gmail.com>
2021-08-27 14:47:50 +0200

    security login

--- src/main/java/com/inti/controller/LoginController.java
+++ src/main/java/com/inti/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.inti.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,15 @@ import com.inti.entity.Utilisateur;
 import com.inti.service.interfaces.IUtilisateurService;
 
 @RestController
-	@RequestMapping(value = "/login")
-	public class LoginController {
-		
-		@Autowired
-		private IUtilisateurService serviceUtilisateur;
-		
-		public Utilisateur login(Principal principal) {
-			return serviceUtilisateur.findByUsername(principal.getName());
-		}
+@RequestMapping(value = "/login")
+public class LoginController {
+
+	@Autowired
+	private IUtilisateurService serviceUtilisateur;
+
+	@CrossOrigin
+	@GetMapping(value = "/user")
+	public Utilisateur login(Principal principal) {
+		return serviceUtilisateur.findByUsername(principal.getName());
+	}
 }
